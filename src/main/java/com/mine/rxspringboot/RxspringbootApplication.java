e486a6a1dea95b57c9b8b8ae3a64eaf8400d5b31
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2019-07-08 19:05:25 +0530

    First commit.
      - data package with:
        - entity and repository packages

---
+++ src/main/java/com/mine/rxspringboot/RxspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.mine.rxspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxspringbootApplication.class, args);
+    }
+
+}
