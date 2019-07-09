b77e32d7971d2f4a049e6fd8876de01a06fd4924
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2019-07-09 20:19:49 +0530

    Added @NoArgsConstructor to User and Organization model classes.

--- src/main/java/com/mine/rxspringboot/data/model/User.java
+++ src/main/java/com/mine/rxspringboot/data/model/User.java
@@ -2,8 +2,10 @@ package com.mine.rxspringboot.data.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class User {
     private String email;
